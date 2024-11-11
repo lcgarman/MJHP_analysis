b87859ba0b90c45022c6ddfe82fa233c2e40327c
Leah Garman <lcgarman@kestrel.local>
2024-11-11 16:17:01 -0600

    made Makefile

--- MJHP_analysis/src/energy_functions.h
+++
@@ -1,6 +0,0 @@
-#ifndef energy_functions_H 
-#define energy_functions_H 
-
-  void find_energy_bounds(Wavefunction * WFK, EnergyStep * ESTP); 
-
-#endif
--- MJHP_analysis/src/recgrid_functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef recgrid_functions_H
-#define recgrid_functions_H  
-
-  void find_HKLgrid_max(NumberGrid *GRD); 
-
-  void prepare_HKLgrid(NumberGrid *GRD);
-
-#endif
